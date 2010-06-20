db5a061969929bb5a7fb86397da06030cf5ad6a5
igorwc@gmail.com <igorwc@gmail.com@0906086e-88c2-11dd-90a9-6306d3ffb32f>
2010-06-20 18:33:17 +0000

    Inclusao de Infra básica de codigo

---
+++ trunk/src/br/seploc/util/GenericDAO.java
@@ -0,0 +1,24 @@
+package br.seploc.util;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.PersistenceContext;
+
+
+public abstract class GenericDAO<T> {
+	@PersistenceContext(unitName = "seploc")
+	protected EntityManagerFactory emf;
+	protected EntityManager em;
+
+	public GenericDAO() {
+		em = PersistenceServiceFactory.getInstance().getManager(); 
+	}
+
+	public abstract void adiciona(T t);
+
+	public abstract T recupera(Integer id);
+
+	public abstract T altera(T t);
+
+	public abstract T remove(Integer id) throws Exception;
+}
---
+++ trunk/src/br/seploc/util/PersistenceServiceFactory.java
@@ -0,0 +1,43 @@
+package br.seploc.util;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class PersistenceServiceFactory {
+    
+    /* EntityManagerFactory */  
+    private static EntityManagerFactory emf ;  
+      
+    /* EntityManager */  
+    private EntityManager        em  ;  
+      
+    /* Singleton PersistenceServiceFactory */  
+    private static PersistenceServiceFactory instance = new PersistenceServiceFactory ();  
+      
+    private PersistenceServiceFactory(){  
+    }  
+    public static PersistenceServiceFactory getInstance() {  
+        return instance;  
+    }  
+  
+public static EntityManagerFactory create() {  
+        try{  
+            if (emf == null)  
+                emf = Persistence.createEntityManagerFactory("seploc");  
+        }catch(Exception e){  
+            System.out.println("Não conseguiu acesar o Banco no metodo create() "+e);  
+        }  
+        return emf;   
+    }  
+  
+public EntityManager getManager() {  
+        try{  
+            emf = create();  
+            em  = emf.createEntityManager();  
+        }catch(Exception e){  
+            System.out.println("Não conseguiu acesar o Banco no metodo getManager() "+e);  
+        }  
+        return em;    
+    }
+}
