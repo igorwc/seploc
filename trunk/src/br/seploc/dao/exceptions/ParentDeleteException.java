13c4f247301ba00c59cbde015a0748e7d63537b9
igorwc@gmail.com <igorwc@gmail.com@0906086e-88c2-11dd-90a9-6306d3ffb32f>
2010-07-03 20:11:53 +0000

    Inclusao de Classes de Exceção

---
+++ trunk/src/br/seploc/dao/exceptions/ParentDeleteException.java
@@ -0,0 +1,26 @@
+package br.seploc.dao.exceptions;
+
+public class ParentDeleteException extends Exception {
+
+	private static final long serialVersionUID = -6321667402700705340L;
+
+	public ParentDeleteException() {
+		
+	}
+
+	public ParentDeleteException(String arg0) {
+		super(arg0);
+		
+	}
+
+	public ParentDeleteException(Throwable arg0) {
+		super(arg0);
+		
+	}
+
+	public ParentDeleteException(String arg0, Throwable arg1) {
+		super(arg0, arg1);
+		
+	}
+
+}
