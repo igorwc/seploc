d2f007e722ff95762cdd42b53785ef8edfc953bd
igorwc@gmail.com <igorwc@gmail.com@0906086e-88c2-11dd-90a9-6306d3ffb32f>
2010-07-03 20:10:30 +0000

    Inclusao de DAOs

---
+++ trunk/src/br/seploc/dao/test/GrupoDAOTest.java
@@ -0,0 +1,107 @@
+package br.seploc.dao.test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import java.util.List;
+
+import junit.framework.Assert;
+
+import org.junit.Test;
+
+import br.seploc.dao.GrupoDAO;
+import br.seploc.dao.exceptions.ParentDeleteException;
+import br.seploc.dao.exceptions.RecordNotFound;
+import br.seploc.pojos.Grupo;
+
+public class GrupoDAOTest {
+
+	@Test
+	public final void testAdicionaGrupo() {
+		GrupoDAO dao = new GrupoDAO();
+		Grupo grupo = new Grupo();
+		grupo.setNomeGrupo("grupo teste");
+		dao.adiciona(grupo);
+
+		grupo = null;
+		grupo = dao.recupera(3);
+		assertNotNull(grupo);
+		assertEquals("grupo teste", grupo.getNomeGrupo());
+
+	}
+
+	@Test
+	public final void testAlteraGrupo() {
+		GrupoDAO dao = new GrupoDAO();
+		Grupo grupo = dao.recupera(3);
+		assertNotNull(grupo);
+		assertEquals("grupo teste", grupo.getNomeGrupo());
+		grupo.setNomeGrupo("teste grupo");
+
+		dao.altera(grupo);
+
+		grupo = null;
+
+		grupo = dao.recupera(3);
+		assertNotNull(grupo);
+		assertEquals("teste grupo", grupo.getNomeGrupo());
+	}
+
+	@Test
+	public final void testRecuperaInteger() {
+		GrupoDAO dao = new GrupoDAO();
+		Grupo grupo = dao.recupera(1);
+		assertNotNull(grupo);
+		org.junit.Assert.assertEquals("Sistema", grupo.getNomeGrupo());
+
+	}
+
+	@Test
+	public final void testRemoveInteger() throws Exception {
+		GrupoDAO dao = new GrupoDAO();
+		Grupo grupo = dao.recupera(3);
+		assertNotNull(grupo);
+		dao.remove(grupo.getCodGrupo());
+
+		grupo = null;
+		grupo = dao.recupera(3);
+
+		Assert.assertNull(grupo);
+	}
+
+	@Test
+	public final void testRemoveRecordNotFound() {
+		GrupoDAO dao = new GrupoDAO();
+
+		try {
+			dao.remove(25);
+		} catch (Exception e) {
+			Assert.assertTrue(e instanceof RecordNotFound);
+		}
+	}
+
+	@Test
+	public final void testRemoveParentDeleteException() {
+		GrupoDAO dao = new GrupoDAO();
+		Grupo grupo = dao.recupera(4);
+		assertNotNull(grupo);
+
+		try {
+			dao.remove(grupo.getCodGrupo());
+		} catch (Exception e) {
+			Assert.assertTrue(e instanceof ParentDeleteException);
+		}
+	}
+
+	@Test
+	public final void testGetLista() {
+		GrupoDAO dao = new GrupoDAO();
+		List<Grupo> lista = dao.getLista();
+
+		for (Grupo item : lista) {
+			System.out.println(item);
+		}
+		Assert.assertTrue(lista.size() == 2);
+	}
+
+}
