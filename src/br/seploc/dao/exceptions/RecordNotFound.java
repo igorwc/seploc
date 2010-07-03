6604d05038774b02ebb2f4e49efc1f6f915ef32b
igorwc@gmail.com <igorwc@gmail.com@0906086e-88c2-11dd-90a9-6306d3ffb32f>
2010-07-03 20:11:53 +0000

    Inclusao de Classes de Exceção

---
+++ src/br/seploc/dao/exceptions/RecordNotFound.java
@@ -0,0 +1,25 @@
+package br.seploc.dao.exceptions;
+
+public class RecordNotFound extends Exception {
+
+	public RecordNotFound() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public RecordNotFound(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public RecordNotFound(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public RecordNotFound(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+
+}
