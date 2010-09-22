5ac0c3480806899ac3c58532bd54a4a90c70f7b7
ghmarinho <ghmarinho@0906086e-88c2-11dd-90a9-6306d3ffb32f>
2010-09-22 22:59:22 +0000

    melhorias

---
+++ src/br/seploc/controllers/ProjetoCB.java
@@ -0,0 +1,99 @@
+package br.seploc.controllers;
+
+import java.io.Serializable;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.component.UIComponent;
+import javax.faces.component.html.HtmlInputText;
+import javax.faces.context.FacesContext;
+import javax.faces.validator.ValidatorException;
+
+import br.seploc.mbeans.ProjetoMB;
+import br.seploc.pojos.Projeto;
+
+public class ProjetoCB implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+	private HtmlInputText inputProjeto;
+	
+	private ProjetoMB projetoMB;	
+	
+	//CONSTRUTOR
+	public ProjetoCB(){
+		this.setProjetoMB(loadProjetoMB());
+	}
+	
+	public ProjetoMB loadProjetoMB() {
+		FacesContext context = FacesContext.getCurrentInstance();
+		ProjetoMB projetoMB = (ProjetoMB) context
+				.getApplication()
+				.evaluateExpressionGet(context, "#{projetoMB}", ProjetoMB.class);
+		return projetoMB;
+	}	
+	
+	// SETTTER AND GETTERS
+	public ProjetoMB getProjetoMB() {
+		return projetoMB;
+	}
+
+
+	public void setProjetoMB(ProjetoMB projetoMB) {
+		this.projetoMB = projetoMB;
+	}
+
+
+	public HtmlInputText getInputProjeto() {
+		return inputProjeto;
+	}
+
+
+	public void setInputProjeto(HtmlInputText inputProjeto) {
+		this.inputProjeto = inputProjeto;
+	}
+
+	// METODOS
+	public List<Projeto> getListaProjetos(){
+		List<Projeto> lista = projetoMB.getLista();
+
+		return lista;
+	}
+	
+	// VALIDATES
+	public void validateNomeProj(FacesContext context, UIComponent component,
+			Object value) throws ValidatorException {
+		if (value == null)
+			return;
+		String nome;
+
+		Pattern pattern = Pattern.compile("^\\s*\\s(\\s)$");
+		Matcher m = pattern.matcher(value.toString());
+		if (value instanceof String)
+			nome = value.toString().trim();
+		else {
+			FacesMessage message = new FacesMessage("Nome do Projeto Inválido");
+			message.setSeverity(FacesMessage.SEVERITY_ERROR);
+			throw new ValidatorException(message);
+		}
+		if (nome.length() < 5) {
+			FacesMessage message = new FacesMessage(
+					"O Nome do Projeto deve ter 5 letras no mínimo");
+			message.setSeverity(FacesMessage.SEVERITY_ERROR);
+			throw new ValidatorException(message);
+		}
+		if (nome.length() >= 80) {
+			FacesMessage message = new FacesMessage(
+					"O Nome do Projeto deve ter entre 5 e 80 caracteres");
+			message.setSeverity(FacesMessage.SEVERITY_ERROR);
+			throw new ValidatorException(message);
+		}
+		if (m.matches()) {
+			FacesMessage message = new FacesMessage(
+					"O Nome do Projeto só tem espaços");
+			message.setSeverity(FacesMessage.SEVERITY_ERROR);
+			throw new ValidatorException(message);
+		}
+	}	
+}
---
+++ src/br/seploc/mbeans/ProjetoMB.java
@@ -0,0 +1,163 @@
+package br.seploc.mbeans;
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+
+import br.seploc.dao.ClienteDAO;
+import br.seploc.dao.ProjetoDAO;
+import br.seploc.dao.exceptions.FieldNotNullException;
+import br.seploc.pojos.Cliente;
+import br.seploc.pojos.Projeto;
+
+public class ProjetoMB implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	static int quantidade = 0;
+	private Projeto projeto;
+	private Cliente cliente;
+	private ProjetoDAO projetoDAO;	
+	ClienteDAO clienteDAO;
+	private String filtroCliente;
+	
+	// CONSTRUTOR
+	/**
+	 * Construtor da classe
+	 */
+	public ProjetoMB(){
+		init();
+	}
+	
+	/**
+	 * Metodo de inicialização
+	 */
+	public void init(){
+		quantidade++;
+		projeto = new Projeto();
+		cliente = new Cliente();
+		cliente.setIdCliente(0);
+		projetoDAO = new ProjetoDAO();
+		clienteDAO = new ClienteDAO();
+		
+	}
+
+	// SETTERS AND GETTERS
+	public Projeto getProjeto() {
+		return projeto;
+	}
+
+	public void setProjeto(Projeto projeto) {
+		this.projeto = projeto;
+	}
+
+	public ProjetoDAO getProjetoDAO() {
+		return projetoDAO;
+	}
+
+	public void setProjetoDAO(ProjetoDAO projetoDAO) {
+		this.projetoDAO = projetoDAO;
+	}
+	
+	public Cliente getCliente() {
+		return cliente;
+	}
+
+	public void setCliente(Cliente cliente) {
+		this.cliente = cliente;
+	}
+
+	public String getFiltroCliente() {
+		return filtroCliente;
+	}
+
+	public void setFiltroCliente(String filtroCliente) {
+		this.filtroCliente = filtroCliente;
+	}
+
+	/**
+	 * Recuperar Lista dos Projetos do cliente
+	 * @return List<OpcionaisReqServ>
+	 */
+	public List<Projeto> getLista() {		  
+		Cliente c = clienteDAO.recupera(cliente.getIdCliente());
+		if (c == null) {
+			return null;
+		} else if (c.getProjetos().isEmpty() || c.getProjetos().size() == 0) {
+			return null;
+		} else {
+			return c.getProjetos();
+		}
+	}
+
+	/**
+	 * Recupera lista de todos os clientes
+	 * @return
+	 */
+	public List<Cliente> getTodosClientes() {		
+		List<Cliente> retorno = this.clienteDAO.getLista();
+		return retorno;
+	}
+	
+	public void cadastrar() {
+		try {
+			if (cliente.getIdCliente() == null || cliente.getIdCliente() == 0) {
+				throw new FieldNotNullException(
+						"Cliente é Obrigatório para Projeto");
+				
+			}else{
+				cliente = clienteDAO.recupera(cliente.getIdCliente());
+				projeto.setCliente(cliente);
+			}
+
+			projetoDAO.adiciona(projeto);
+
+			addGlobalMessage("Inclusão feita com sucesso!");
+		} catch (Exception e) {
+			addGlobalMessage(e.getMessage());
+			e.printStackTrace();
+		}
+		limpar();
+	}
+
+	/**
+	 * Editar o projeto
+	 */
+	public void editar() {
+		projeto = projetoDAO.recupera(projeto.getCodProj());
+	}
+
+	/**
+	 * Apagar o projeto
+	 */	
+	public void apagar() {
+		try{
+			projetoDAO.remove(projeto.getCodProj());
+			addGlobalMessage("Projeto excluído com sucesso!");
+		} catch (Exception e) {
+			addGlobalMessage(e.getMessage());
+		}
+		this.limpar();
+	}
+
+	/**
+	 * Limpar os Limpar
+	 */
+	public void limpar() {
+		cliente = new Cliente();
+		projeto = new Projeto();
+	}	
+	
+	/**
+	 * Método para incluir mensagens globais no formulário de cadastro
+	 * 
+	 * @param String
+	 *            message
+	 */
+	public static void addGlobalMessage(String message) {
+		FacesMessage facesMessage = new FacesMessage(message);
+		FacesContext.getCurrentInstance().addMessage(null, facesMessage);
+	}	
+
+}
