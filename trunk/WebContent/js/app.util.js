8770c6e7cc6ce084e5071e429912c069dda581ba
ghmarinho <ghmarinho@0906086e-88c2-11dd-90a9-6306d3ffb32f>
2010-09-29 04:13:33 +0000

    melhorias

--- trunk/WebContent/js/app.util.js
+++ trunk/WebContent/js/app.util.js
@@ -12,8 +12,10 @@ function showFilled(Value)
 
 function showData()
 {
-	var day_nme = new Array("Domingo", "Segunda-feira", "Terça-feira", "Quarta-feira", "Quinta-feira", "Sexta-feira", "Sábado");
-	var month_nme = new Array("", "Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro");
+	var day_nme = new Array("Domingo", "Segunda-feira", "Ter\u00E7a-feira", "Quarta-feira", "Quinta-feira", "Sexta-feira", "S\u00E1bado");
+	var month_nme = new Array("", "Janeiro", "Fevereiro", "Mar\u00E7o", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro");
+	//var day_nme = new Array("Domingo", "Segunda-feira", "Terça-feira", "Quarta-feira", "Quinta-feira", "Sexta-feira", "Sábado");
+	//var month_nme = new Array("", "Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro");
 	var now = new Date();
 	var nowDay = now.getDay();
 	var nowDate = now.getDate();
