71ef81b7e0d8eb3e6aef10f0852b14e39d313f3b
ghmarinho <ghmarinho@0906086e-88c2-11dd-90a9-6306d3ffb32f>
2010-09-30 03:20:49 +0000

    melhorias

--- trunk/src/br/seploc/dao/test/RequisicaoServicoDAOTest.java
+++ trunk/src/br/seploc/dao/test/RequisicaoServicoDAOTest.java
@@ -6,8 +6,11 @@ import junit.framework.Assert;
 import org.junit.Test;
 
 import br.seploc.dao.OpcionaisReqServDAO;
+import br.seploc.dao.PapelDAO;
 import br.seploc.dao.RequisicaoServicoDAO;
+import br.seploc.pojos.LinhaRequisicao;
 import br.seploc.pojos.OpcionaisReqServ;
+import br.seploc.pojos.Papel;
 import br.seploc.pojos.ReqServicosOpcionais;
 import br.seploc.pojos.RequisicaoServico;
 
@@ -59,6 +62,30 @@ public class RequisicaoServicoDAOTest {
 			System.out.println(rso);
 		}
 	}
+	
+	@Test
+	public final void testAddLinha() throws Exception {
+		RequisicaoServicoDAO dao = new RequisicaoServicoDAO();
+		RequisicaoServico rq = dao.recupera(1);
+		LinhaRequisicao lr = new LinhaRequisicao();
+		PapelDAO pd = new PapelDAO();
+		Papel p = pd.recupera(1);
+		lr.setNomeArquivo("TESTE-00");
+		lr.setDimensao(1.0);
+		lr.setFormato(1.0);
+		lr.setImpressao("Color");
+		lr.setQuant(1);		
+		lr.setPapel(p);
+		dao.addLinha(rq, lr);
+		dao.altera(rq);
+		rq = null;
+		rq = dao.recupera(1);
+		Assert.assertTrue(rq.getLinhaRequisicao().size() == 2);
+		System.out.println(rq.getLinhaRequisicao().size());
+		for(LinhaRequisicao lrs :rq.getLinhaRequisicao()){
+			System.out.println(lrs);
+		}
+	}	
 	
 	@Test
 	public final void testAddOpcionalRepetido() throws Exception {
