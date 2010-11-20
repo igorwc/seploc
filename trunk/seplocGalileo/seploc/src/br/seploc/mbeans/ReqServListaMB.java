d3f65ffa36c73029a646f27a40e02d44993beed2
ghmarinho@gmail.com <ghmarinho@gmail.com@0906086e-88c2-11dd-90a9-6306d3ffb32f>
2010-11-20 02:50:54 +0000

    melhorias

--- trunk/seplocGalileo/seploc/src/br/seploc/mbeans/ReqServListaMB.java
+++ trunk/seplocGalileo/seploc/src/br/seploc/mbeans/ReqServListaMB.java
@@ -1,24 +1,13 @@
 package br.seploc.mbeans;
 
 import java.io.Serializable;
-import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.List;
 
 import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
 
-import br.seploc.dao.ClienteDAO;
-import br.seploc.dao.EntregaDAO;
-import br.seploc.dao.OpcionaisReqServDAO;
-import br.seploc.dao.PapelDAO;
 import br.seploc.dao.RequisicaoServicoDAO;
-import br.seploc.pojos.Cliente;
-import br.seploc.pojos.Entrega;
-import br.seploc.pojos.LinhaRequisicao;
-import br.seploc.pojos.OpcionaisReqServ;
-import br.seploc.pojos.Papel;
-import br.seploc.pojos.Projeto;
 import br.seploc.pojos.RequisicaoServico;
 
 public class ReqServListaMB implements Serializable {
@@ -26,20 +15,6 @@ public class ReqServListaMB implements Serializable {
 	private static final long serialVersionUID = 1L;
 	private RequisicaoServico reqServico;
 	private RequisicaoServicoDAO reqServicoDAO;
-	private Cliente cliente;
-	private OpcionaisReqServ opcional;
-	private Entrega entrega;
-	private Papel papel;
-	private Projeto projeto;
-	private LinhaRequisicao linhaReqServ;
-	private String nomePapel;
-	private String filtroOpcional;
-	private String filtroEntrega;
-	private String filtroProjeto;
-	private String filtroCliente;
-	private String valorTotalReq;
-	private int quantidadeOpcional;
-	private int numReqAtual;
 	private int numReqBusca;
 	
 	// CONSTRUTOR
@@ -47,17 +22,7 @@ public class ReqServListaMB implements Serializable {
 	 * Construtor da classe
 	 */
 	public ReqServListaMB() {
-		cliente = new Cliente();
-		cliente.setIdCliente(0);
-		opcional = new OpcionaisReqServ();
-		entrega = new Entrega();
-		papel = new Papel();
-		projeto = new Projeto();
-		linhaReqServ = new LinhaRequisicao();
-		setReqServico(new RequisicaoServico());
-		reqServicoDAO = new RequisicaoServicoDAO();
-		filtroCliente = "";
-		valorTotalReq = "0,00";		
+		reqServicoDAO = new RequisicaoServicoDAO();		
 	}
 
 	// GETTERS E SETTERS
@@ -77,118 +42,6 @@ public class ReqServListaMB implements Serializable {
 		this.reqServicoDAO = reqServicoDAO;
 	}
 
-	public Cliente getCliente() {
-		return cliente;
-	}
-
-	public void setCliente(Cliente cliente) {
-		this.cliente = cliente;
-	}
-
-	public OpcionaisReqServ getOpcional() {
-		return opcional;
-	}
-
-	public void setOpcional(OpcionaisReqServ opcional) {
-		this.opcional = opcional;
-	}
-
-	public Entrega getEntrega() {
-		return entrega;
-	}
-
-	public void setEntrega(Entrega entrega) {
-		this.entrega = entrega;
-	}
-
-	public Papel getPapel() {
-		return papel;
-	}
-
-	public void setPapel(Papel papel) {
-		this.papel = papel;
-	}
-
-	public Projeto getProjeto() {
-		return projeto;
-	}
-
-	public void setProjeto(Projeto projeto) {
-		this.projeto = projeto;
-	}
-
-	public LinhaRequisicao getLinhaReqServ() {
-		return linhaReqServ;
-	}
-
-	public void setLinhaReqServ(LinhaRequisicao linhaReqServ) {
-		this.linhaReqServ = linhaReqServ;
-	}
-
-	public String getNomePapel() {
-		return nomePapel;
-	}
-
-	public void setNomePapel(String nomePapel) {
-		this.nomePapel = nomePapel;
-	}
-
-	public String getFiltroOpcional() {
-		return filtroOpcional;
-	}
-
-	public void setFiltroOpcional(String filtroOpcional) {
-		this.filtroOpcional = filtroOpcional;
-	}
-
-	public String getFiltroEntrega() {
-		return filtroEntrega;
-	}
-
-	public void setFiltroEntrega(String filtroEntrega) {
-		this.filtroEntrega = filtroEntrega;
-	}
-
-	public String getFiltroProjeto() {
-		return filtroProjeto;
-	}
-
-	public void setFiltroProjeto(String filtroProjeto) {
-		this.filtroProjeto = filtroProjeto;
-	}
-
-	public String getFiltroCliente() {
-		return filtroCliente;
-	}
-
-	public void setFiltroCliente(String filtroCliente) {
-		this.filtroCliente = filtroCliente;
-	}
-
-	public String getValorTotalReq() {
-		return valorTotalReq;
-	}
-
-	public void setValorTotalReq(String valorTotalReq) {
-		this.valorTotalReq = valorTotalReq;
-	}
-
-	public int getQuantidadeOpcional() {
-		return quantidadeOpcional;
-	}
-
-	public void setQuantidadeOpcional(int quantidadeOpcional) {
-		this.quantidadeOpcional = quantidadeOpcional;
-	}
-
-	public int getNumReqAtual() {
-		return numReqAtual;
-	}
-
-	public void setNumReqAtual(int numReqAtual) {
-		this.numReqAtual = numReqAtual;
-	}
-
 	public int getNumReqBusca() {
 		return numReqBusca;
 	}
@@ -197,55 +50,8 @@ public class ReqServListaMB implements Serializable {
 		this.numReqBusca = numReqBusca;
 	}
 	
-	public List<Cliente> getTodosClientes() {
-		ClienteDAO clienteDAO = new ClienteDAO();
-		List<Cliente> retorno = clienteDAO.getLista();
-		filtroCliente = "";
-		return retorno;
-	}
-
-	public List<Papel> getTodosPapeis() {
-		PapelDAO papelDAO = new PapelDAO();
-		List<Papel> retorno = papelDAO.getLista();
-
-		return retorno;
-	}
-
-	public List<OpcionaisReqServ> getTodosOpcionais() {
-		OpcionaisReqServDAO opcionalDAO = new OpcionaisReqServDAO();
-		List<OpcionaisReqServ> retorno = opcionalDAO.getLista();
-
-		return retorno;
-	}
-
-	public List<Entrega> getTodasEntregas() {
-		EntregaDAO entregaDAO = new EntregaDAO();
-		List<Entrega> retorno = entregaDAO.getLista();
-
-		return retorno;
-	}
-
-	public List<Projeto> getTodosProjetos() {
-
-		List<Projeto> retorno = null;
-		if (cliente == null || cliente.getIdCliente().intValue() == 0) {
-			retorno = new ArrayList<Projeto>();
-		} else {
-			if(cliente.getProjetos().isEmpty()){
-				retorno = new ArrayList<Projeto>();
-				Projeto p = new Projeto();
-				p.setCodProj(0);
-				p.setProjeto("Cliente n�o tem projetos");
-				retorno.add(p);
-			}else{
-				retorno = cliente.getProjetos();
-			}
-		}
-		return retorno;
-	}
-
 	public List<RequisicaoServico> getListaReqServ() {
-		// setar data de 60 dias atr�s
+		// setar data de 60 dias atras
 		Calendar calendarData = Calendar.getInstance();
 		  int numeroDiasParaSubtrair = -60;
 		  calendarData.add(Calendar.DATE, numeroDiasParaSubtrair);
@@ -254,13 +60,6 @@ public class ReqServListaMB implements Serializable {
 		List<RequisicaoServico> retorno = reqServicoDAO.getListaSinceDate(dias60);
 		return retorno;
 	}
-	
-	public List<RequisicaoServico> getTodasReqServPorCliente(Cliente cliente) {
-		List<RequisicaoServico> retorno = reqServicoDAO
-				.getListaPorProjeto(cliente);
-
-		return retorno;
-	}	
 	
 	public void editar(){
 		try{
@@ -272,11 +71,23 @@ public class ReqServListaMB implements Serializable {
 	}
 	
 	public void apagar(){
+		try{
+			reqServicoDAO.remove(this.reqServico.getNumReq());
+			addGlobalMessage("Requisição Excluido!");
+		} catch (Exception e) {
+			e.printStackTrace();
+			addGlobalMessage(e.getMessage());
+		}
 		
 	}
 	
 	public void imprimir(){
-		
+		try{
+			reqServico = reqServicoDAO.recupera(reqServico.getNumReq());
+		} catch (Exception e) {
+			e.printStackTrace();
+			addGlobalMessage(e.getMessage());
+		}				
 	}
 	
 	public void mostrar(){
